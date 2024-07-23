201aa3e6141f2214056d187f7f1037e7f43dbfdb
raj shringi <raj24shringi@gmail.com>
2024-07-23 19:56:03 +0530

    Add About and Skills Component in Main page and change some style in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import About from "@/components/About";
 import Navbar from "@/components/Navbar";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="bg-gray-50 h-screen">
-      <div className="max-w-[800px] mx-auto py-6 px-4 text-stone-800 flex flex-col gap-6">
+    <main className="bg-gray-50 h-screen overflow-auto">
+      <div className="max-w-[700px] mx-auto py-6 px-4 text-stone-800 flex flex-col gap-12">
         <Navbar />
+        <About />
+        <Skills />
       </div>
     </main>
   );
