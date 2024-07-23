7a1a4e3efd6287f203eea7ac2f04a3f9ec988618
raj shringi <raj24shringi@gmail.com>
2024-07-23 17:50:17 +0530

    add horizontal padding for mobile screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="bg-gray-50 h-screen">
-      <div className="max-w-[800px] mx-auto py-6 text-stone-800 flex flex-col gap-6">
+      <div className="max-w-[800px] mx-auto py-6 px-4 text-stone-800 flex flex-col gap-6">
         <Navbar />
       </div>
     </main>
