8392fd8e5f9ca914c6fe1aafd071f0f13c9021ef
raj shringi <raj24shringi@gmail.com>
2024-07-23 17:45:32 +0530

    give style to main page and add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import About from "@/components/About";
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="bg-gray-50 h-screen">
+      <div className="max-w-[800px] mx-auto py-6 text-stone-800 flex flex-col gap-6">
+        <Navbar />
+      </div>
+    </main>
+  );
 }
