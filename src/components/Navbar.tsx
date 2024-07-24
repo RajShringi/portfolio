58592231524392577310d27e3bed54c7e1d066e1
raj shringi <raj24shringi@gmail.com>
2024-07-24 21:44:23 +0530

    add social links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,22 +12,22 @@ export default function Navbar() {
     <nav className="flex items-center justify-center sm:justify-between flex-wrap">
       <h2 className="text-lg tracking-widest from-left">RS</h2>
       <ul className="flex items-center gap-10 from-right">
-        <Link href={"/"}>
+        <Link href={"https://x.com/RajShringi1"} target="_blank">
           <li className="bg-transparent rounded-full p-2 hover:bg-stone-200">
             <FaXTwitter className="text-2xl" />
           </li>
         </Link>
-        <Link href={"/"}>
+        <Link href={"https://www.linkedin.com/in/raj-shringi/"} target="_blank">
           <li className="bg-transparent rounded-full p-2 hover:bg-stone-200">
             <FaLinkedinIn className="text-2xl" />
           </li>
         </Link>
-        <Link href={"/"}>
+        <Link href={"https://github.com/RajShringi"} target="_blank">
           <li className="bg-transparent rounded-full p-2 hover:bg-stone-200">
             <FaGithub className="text-2xl" />
           </li>
         </Link>
-        <Link href={"/"}>
+        <Link href={"https://rajshringi.hashnode.dev/"} target="_blank">
           <li className="bg-transparent rounded-full p-2 hover:bg-stone-200">
             <FaHashnode className="text-2xl" />
           </li>
