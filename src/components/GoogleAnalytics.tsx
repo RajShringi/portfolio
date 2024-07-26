5a66f736188ea5e7086e1d289111cd5d1130a06d
raj shringi <raj24shringi@gmail.com>
2024-07-26 14:40:55 +0530

    change how script tag is define in GoogleAnalytics component

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -6,19 +6,15 @@ const GoogleAnalytics = () => {
     <head>
       <Script
         async
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
       ></Script>
-
-      <Script id="" strategy="afterInteractive">
+      <Script>
         {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');`}
       </Script>
     </head>
   );
